7a6aaad96d9951d94e953b2e989cf0d94b04b45f
黎海冬 <haidong.li@dfactory.tech>
2021-10-20 15:52:13 +0800

    release app

---
+++ static/js/52.ef04a5df.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[52],{1306:function(n,e,a){},1353:function(n,e,a){"use strict";a.r(e),a.d(e,"default",(function(){return t}));a(6);var c=a(992),i=(a(1306),a(18));function t(n){var e=n.lang;return Object(i.jsx)("div",{className:"home_box",children:Object(i.jsx)(c.a,{lang:e})})}}}]);
+//# sourceMappingURL=52.ef04a5df.chunk.js.map
---
+++ static/js/56.3e6dd29d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[56],{1328:function(n,a,e){},1366:function(n,a,e){"use strict";e.r(a),e.d(a,"default",(function(){return s}));var c=e(992),t=(e(1328),e(781)),i=e(18);function s(n){var a=n.lang;return Object(i.jsxs)("div",{className:"home_box",children:[Object(i.jsx)(c.a,{lang:a}),Object(i.jsx)(t.a,{lang:a})]})}}}]);
+//# sourceMappingURL=56.3e6dd29d.chunk.js.map
