fff6a6ba62a0f4625911a44665d7d4a6e588a9a0
wenchao.huang <wenchao.huang@dfactory.tech>
2021-12-14 02:11:10 +0800

    release app

---
+++ static/js/62.451f8ef8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[62],{1415:function(n,e,a){},1466:function(n,e,a){"use strict";a.r(e),a.d(e,"default",(function(){return t}));a(13);var c=a(1098),i=(a(1415),a(32));function t(n){var e=n.lang;return Object(i.jsx)("div",{className:"home_box",children:Object(i.jsx)(c.a,{lang:e})})}}}]);
+//# sourceMappingURL=62.451f8ef8.chunk.js.map
